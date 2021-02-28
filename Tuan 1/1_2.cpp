b8d178a01c7b3ec088e57484f6e1fb9729db1696
phamxuansang241 <79781763+phamxuansang241@users.noreply.github.com>
2021-02-28 14:50:52 +0700

    Add files via upload

---
+++ Tuan 1/1_2.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+string name;
+int main() {
+    cout << "Nhap vao ten ban: ";
+    cin >> name;
+    cout << "Chao " << name << " den voi the gioi lap trinh.";
+    return 0;
+}
+
